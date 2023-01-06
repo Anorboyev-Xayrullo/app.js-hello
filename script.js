2f89880eaea5598fdf7eeb96931e81ccef40a535
Anorboyev-Xayrullo <anorboyevxayrullo841@gmail.com>
2023-01-06 10:27:50 +0500

    app.js

---
+++ script.js
@@ -0,0 +1,478 @@
+// // DOM = document object model
+// console.log(document);
+// const p = document.getElementBuId
+// const button = document.querySelector("#click");
+
+// p.innerHTML ="<span class='something'>ajdoaijoi hosdhuh</span>"
+// p.textContent = "this is just a text";
+// p.className = "new-class"
+// console.log(p)
+// console.log(button);
+
+
+// var a = 5
+// var b = 4
+// console.log(b+a);
+
+
+// ar b = 4
+// console.log(a)
+
+
+
+// const e = 9
+// const f = 3
+// console.log(e/f)var a = "XAYRULLOX"
+
+
+// let name = "Xayrullo"
+// age = 19
+// console.log('mening ismim'  + name + ' yoshim ' + age +  ' da ' + name + 'ismim'  )
+
+// let jami = 25
+// var kelganlar = 15
+
+// console.log("jami oquvchilar 25 ta. kelganlar 15 ta. kelmaganlar 10 ta")
+// console.log( )
+
+
+
+// let x = 5
+//  let y = 8
+//  console.log(x+y)
+
+
+// let a = 4
+// let a = 4*1
+// let a = 4*1
+// let a = 4*1
+// let a = 4*1
+// let a = 4*1
+// let a = 4*1
+// let a = 4*1
+// let a = 4*1
+// let a = 4*1
+
+// console.log("malika = Malika")
+
+
+
+// a = 5
+// b = "6"
+// c = 7
+// console.log(a+b+c)
+
+
+// var a = "malika"
+// let  b = "Sojida"
+// const c = "sojida"
+// console.log(b>c)
+
+// var a = 5
+// let b =6
+// const c = 7
+// console.log(b<c)
+
+
+// var b = 4
+
+// let h = 1
+// let c = 2
+// let d = 3
+// let e = 4
+// let f = 5
+// let g = 6
+// let i = 7
+// let j = 8
+// let k = 9
+// console.log(b*h)
+// console.log(b*c)
+// console.log(b*d)
+// console.log(b*e)
+// console.log(b*f)
+// console.log(b*g)
+// console.log(b*i)
+// console.log(b*j)
+// console.log(b*k)
+
+
+
+
+
+// var a = 5
+// let b = 6 
+// const c = 7
+// console.log(b>c)
+
+
+
+// var = "xayrullox"
+// let = "Xayrullox"
+// console.log('xayrullox' > 'Xayrullox')
+
+// let a =10
+// let b = a + 1
+// let c = a - 2
+// if (a>0) {
+//     console.log(c)    
+// }
+
+// else {
+//     console.log(0)
+// }
+
+
+// let number = -4
+// if (number > 0) {
+//     console.log (number + 1);
+// }
+// else {console.log(number -2);
+// }
+
+
+// let number = 0
+// if (number > 0 ){
+//     console.log(number + 1);
+// }
+// else if (number < 0 ){
+//     console.log( number - 2);
+// }
+// else { console.log(number + 10);}
+
+
+// " ", '',```,
+// let name = "Ozbekiston"
+// // let name2 = 'o'zbekiston
+// let ism = 'Xayrullox'
+// let name3 = 'Mening ismim ${ism}'
+// console.log( name  [name.length - 1]+' ' +ism + ' asia ');
+
+
+
+// let  a = 5
+//  if ( a == 1) {
+//     console.log("Yanvar");
+//  }
+//  else if ( a == 2) {
+//    console.log("Fevral");  
+// }
+// else if ( a == 3){
+//     console.log("Mart");
+// }
+// else if ( a == 4) {
+//     console.log("Aprel");
+// }
+// else if ( a == 5) {
+//     console.log("May");
+// }
+// else if ( a == 6) {
+//     console.log("Iyun");
+// }
+// else if ( a == 7) {
+//     console.log("Iyun");
+// }
+// else if ( a == 8) {
+//     console.log("Avgust");
+// }
+// else if ( a == 9) {
+//     console.log("Sentyabr");
+// }
+// else if ( a == 10) {
+//     console.log("Oktyabr");
+// }
+// else if ( a == 11) {
+//     console.log("Noyabr");
+// }
+// else if ( a == 12) {
+//     console.log ("Dekabr");
+// }
+// else {
+//     console.log("Mavjud emas");}
+
+
+
+// let word = "tarvuz"
+// let size = word.length - 1 
+// console.log('juft son') {
+//     else{
+//         console.log('toq son')
+//     }
+// }
+// let word = "tarvuz"
+// if(word.length - 1 % 2 == 0) 
+// console.log('juft son') {
+//     else{
+//         console.log('toq son')
+//     }
+// }
+       
+    
+
+// let number = 9
+// if (number> 0 && number <= 7) {
+//     if (number == 1) {
+//         console.log('Dushanba')
+//     }
+// }
+// else if (number == 2) {
+//     console.log('Seshanba')
+// }
+
+// let day = 'Payshanba'
+// if (day == 'Dushanba') {
+//     console.log(1);
+// }
+// else if(day == 'Seshanba') {
+//     console.log(2)
+// }
+// else if (day == 'CHorshanba') {
+//     console.log(3);
+// }
+// else if (day == 'Payshanba') {
+//     console.log(4);
+// }
+// else if (day == 'Juma') {
+//     console.log(5);
+// }
+// else if (day == SHanba) {
+//     console.log(6);
+// }
+// else if (day == Yakshanba) {
+//     console.log(7);
+// }
+
+
+// for(let i = 10; i >= 0; i --) {
+//     console.log(i);
+// }
+// for(let i = 1; i<= 5; i+=3) {
+//     console.log(i);
+// }
+
+// for(let i = 10; i >= 0; i --) {
+//     console.log(i);
+// 
+
+
+
+
+//  for(let i = 1; i <= 10; i++) {
+// if(i % 2 != 0) {
+    // console.log(i, 'toq son');
+//  }
+//  else{
+    // console.log(i, 'juft son');
+//  }
+// }
+
+
+
+
+
+// let a = 7
+// if(a % 2 != 0) {
+// console.log("toq son")
+// }
+// else {
+//     console.log("juft son")
+// }
+
+
+
+
+// for(i = 10; i <= 100; i++)
+// if(i % 2 != 0) {
+//     console.log(i,"toq son")
+// }
+// else{
+//     console.log(i,"juft son")
+// }
+// let a =3
+// let b = 10
+// let c = 0
+// if(a<b){
+//     for(let i =a;i<=b;i++){
+        
+//     }
+//     console.log(c)
+// }
+
+
+
+// let a = 1
+// let b = 5
+// let c = 1
+// if(a < b) {
+//     for(let i = a; i <= b;i*=) {
+//         c*+ 1
+//     }
+// 
+// console.log(c)
+
+
+
+// for(i = 10; i <= 200; i +10) {
+//     console.log(i)
+// }
+
+
+
+// var a = prompt( "a tomonni kiriting")
+// var b = prompt( "b tomonni kiriting")
+// var 5 = a*b
+// console.log(5)
+// var a = 10
+// var b = 4
+// p ( a + b ) * 2       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let name = "Xayrullo"
+// let age = 18
+// console.log(" Mening ismim "+ name + " mening yoshim  " + age + " da");
+// let a = "5"
+// let b = +"5"
+// console.log(+a+b)
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// const num1 = 5;
+// const num2 = 3;
+// // add two numbers
+// const sum = num1 + num2;
+// // display the sum
+// console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+// let a = prompt("birinchi sonni kiriting")
+// // let b = prompt("ikkinchi sonni kiriting")
+// console.log(typeof +a);
+
+
+
+
+// var a = 5
+// var b = 9
+// console.log(a+b);
+
+
+
+// var age = 
+// console.log('Mening ismim Xayrullo yoshim 18 da);
+
+
+// 05.01.2023-yil   
+// mavzu:Hello
+
+
+// // 20 == 20 || 12 === "12" ? console.log(true) : console.log(false);
+// null == "number" ? console.log(true) :console.log(false);
+// console.log(+null);
+
+
+
+
+// for (let i = 0; i <= 10; i ++ )
+// if (i%2==0){
+//     console.log("juft son");
+// }
+//     else{
+//         console.log("toq son");
+//     }
+
+
+//   let name = "Xayrullo"
+//   let age = 18
+// console.log("Mening ismim " + name + " yoshim " + age + "da");  
+  
+
+
+
+
+// let name = "Xayrullox"
+// let age = 18
+// console.log('mening ismim '+ name + ' yoshim ' + age + " da");
+
+
+// let a = +"5"
+// let b = 5
+// console.log(a+b);
+// console.log(a/b);
+// console.log(a*b);
+// console.log(a-b);
+
+
+
+// let b = 5
+// console.log(b**2);
+
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+
+// console.log((a+b)*2);
+
+
+
+// let  a = "6"
+// let  b = 2
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// for (let i = 0; i <= 10; i = i + 1){
+//     if(i){
+//         if (i,  "if");
+//         console.log(i ,);
+//     }else{
+//         console.log(i , "else");
+//     }
+// }
+// +null === 0 ? console.log("true"): console.log("false");
+
+
+// if (name == "ali") {
+//     var name = "Ali"
+//     console.log(name);
+// }else {
+    
+//     console.log(name, "else");
+// 
+
+// var age = "32"
+// console.log(typeof age);
+
+
+
